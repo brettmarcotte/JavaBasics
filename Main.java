86d45638679d6bd82982132cd69a090bc3ce905f
brettmarcotte <87341291+brettmarcotte@users.noreply.github.com>
2022-01-03 20:37:11 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.io.StringBufferInputStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String firstName = "Brett";
+    String lastName = "Marcotte";
+    String fullName = lastName + firstName;
+
+    }
+}
