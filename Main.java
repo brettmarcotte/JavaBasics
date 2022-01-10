2759611295afbe3c97841ae8fff11ba1a21d27cd
brettmarcotte <87341291+brettmarcotte@users.noreply.github.com>
2022-01-10 18:29:39 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,17 +1,35 @@
-package com.company;
-
 public class Main {
-
-    public static String main(String[] args) {
-        String firstName = "Brett";
-        String lastName = "Marcotte";
-        String fullName = lastName + firstName;
-        System.out.println(fullName);
-        return fullName;
-
+    public class PerValueOrSum {
+        public static boolean makesTen(int a, int b) {
+            if(a + b == 10 || a == 10 || b == 10){
+                return true;
+            }
+            return false;
+        }
     }
-    public static String createFullName(String fName, String lName){
-       return null;
+    public class Challenge {
+        public static int numberSyllables(String word) {
+            int count = 1;
+            for(int i = 0; i < word.length();i++){
+                if(word.charAt(i) == '-') count++;
+            }
+            return count;
+        }
+    }
+    public class Challenge {
+        public static boolean match(String str1, String str2) {
+            return str1.equalsIgnoreCase(str2);
+        }
+    }
+    public class Challenge {
+        public static String hackerSpeak(String str) {
+            return str
+                    .replace('a','4')
+                    .replace('e','3')
+                    .replace('i','1')
+                    .replace('o','0')
+                    .replace('s','5');
+        }
     }
 
 }
